090ec87187a900c5dfa86c80ddba3e26d1a75a5c
Vinicius Gerevini <viniciusgerevini@gmail.com>
2021-12-03 08:27:38 +1100

    add a 5 pixel offset as examples jitter sometimes in big screens

--- bt_example/actions/go_to_target_position.gd
+++ bt_example/actions/go_to_target_position.gd
@@ -10,7 +10,7 @@ func tick(actor, blackboard):
 
 	actor.move_towards_position(target_position, blackboard.get("delta"))
 
-	if actor.position.distance_to(target_position) < 1:
+	if actor.position.distance_to(target_position) < 5:
 		return SUCCESS
 
 	return RUNNING
